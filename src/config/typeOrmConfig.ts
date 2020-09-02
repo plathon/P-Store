ced83e499d21fd0ede6de538330853158e9f5c50
= <=>
2020-09-02 17:17:15 -0300

    :clown_face: crieate .env file
    add .env file to development environment but ready to add more

---
+++ src/config/typeOrmConfig.ts
@@ -0,0 +1,13 @@
+import { TypeOrmModuleOptions } from '@nestjs/typeorm';
+
+export const typeOrmConfig: TypeOrmModuleOptions = {
+  type: 'postgres',
+  host: process.env.PG_HOST,
+  port: 5432,
+  username: process.env.PG_USERNAME,
+  password: process.env.PG_PASSWORD,
+  database: process.env.PG_DATABASE,
+  entities: [__dirname + '/../**/*.entity{.ts,.js}'],
+  synchronize: true,
+  logging: true,
+};
--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,4 @@
+require('dotenv-flow').config();
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
