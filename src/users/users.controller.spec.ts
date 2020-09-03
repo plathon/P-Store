491b7b496eea707f875f092de9e83a3ce7be6fe7
= <=>
2020-09-02 23:00:38 -0300

    :white_check_mark: update tests dependecies

--- src/users/users.controller.spec.ts
+++ src/users/users.controller.spec.ts
@@ -1,15 +1,28 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { UsersController } from './users.controller';
+import { UsersService } from './users.service';
+import { UserRepository } from './user.repository';
+
+const mockUserRepository = () => ({});
 
 describe('UsersController', () => {
   let controller: UsersController;
+  let usersService: UsersService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [UsersController],
+      providers: [
+        UsersService,
+        {
+          provide: UserRepository,
+          useFactory: mockUserRepository,
+        },
+      ],
     }).compile();
 
-    controller = module.get<UsersController>(UsersController);
+    controller = await module.get<UsersController>(UsersController);
+    usersService = await module.get<UsersService>(UsersService);
   });
 
   it('should be defined', () => {
--- src/users/users.service.spec.ts
+++ src/users/users.service.spec.ts
@@ -1,12 +1,21 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { UsersService } from './users.service';
+import { UserRepository } from './user.repository';
+
+const mockUserRepository = () => ({});
 
 describe('UsersService', () => {
   let service: UsersService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [UsersService],
+      providers: [
+        UsersService,
+        {
+          provide: UserRepository,
+          useFactory: mockUserRepository,
+        },
+      ],
     }).compile();
 
     service = module.get<UsersService>(UsersService);
