5b88d192444fc90476effa2470ed73c7fecc0c20
= <=>
2020-09-02 21:44:27 -0300

    :construction: encrypt user password on register workflow

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -2,6 +2,7 @@ import { CreateUserRequestDTO, CreateUserResponseDTO } from './users.dto';
 import { Injectable } from '@nestjs/common';
 import { UserRepository } from './user.repository';
 import { User } from './user.entity';
+import { genSalt, hash } from 'bcryptjs';
 
 @Injectable()
 export class UsersService {
@@ -10,10 +11,11 @@ export class UsersService {
     createUserRequestDTO: CreateUserRequestDTO,
   ): Promise<CreateUserResponseDTO> {
     const { name, email, password } = createUserRequestDTO;
+    const salt = await genSalt();
     const user = new User();
     user.name = name;
     user.email = email;
-    user.password = password;
+    user.password = await hash(password, salt);
     const response = await user.save();
     const { id } = response;
     const createUserResponseDTO: CreateUserResponseDTO = { id, name, email };
